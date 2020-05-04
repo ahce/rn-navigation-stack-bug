f088dc262557dd7d0b4bcdb96c4293f102656102
Alejandro <alejandroce@lagash.com>
2020-05-03 20:54:13 -0400

    update with setOptions behavior

--- App.js
+++ App.js
@@ -6,19 +6,30 @@
  * @flow strict-local
  */
 
-import React, {useState} from 'react';
+import React, {useCallback, useRef} from 'react';
 import {Button, ScrollView} from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
+import {NavigationContainer, useFocusEffect} from '@react-navigation/native';
 import {createStackNavigator, TransitionPresets} from '@react-navigation/stack';
 
 import {Header, LearnMoreLinks} from 'react-native/Libraries/NewAppScreen';
 
-const Screen1 = ({navigation}) => (
-  <>
-    <Header />
-    <Button title="NAVIGATE" onPress={() => navigation.navigate('Screen2')} />
-  </>
-);
+const Screen1 = ({navigation}) => {
+  const countRef = useRef(0);
+
+  useFocusEffect(
+    useCallback(() => {
+      countRef.current += 1;
+      navigation.setOptions({title: `Title ${countRef.current}`});
+    }, [navigation]),
+  );
+
+  return (
+    <>
+      <Header />
+      <Button title="NAVIGATE" onPress={() => navigation.navigate('Screen2')} />
+    </>
+  );
+};
 
 const Screen2 = () => (
   <ScrollView>
@@ -28,22 +39,18 @@ const Screen2 = () => (
 
 const Stack = createStackNavigator();
 
-const App = () => {
-  const [navigationState, setNavigationState] = useState();
-
-  return (
-    <NavigationContainer onStateChange={setNavigationState}>
-      <Stack.Navigator
-        initialRouteName="Screen1"
-        screenOptions={{
-          ...TransitionPresets.SlideFromRightIOS,
-          gestureEnabled: false,
-        }}>
-        <Stack.Screen name="Screen1" component={Screen1} />
-        <Stack.Screen name="Screen2" component={Screen2} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-};
+const App = () => (
+  <NavigationContainer>
+    <Stack.Navigator
+      initialRouteName="Screen1"
+      screenOptions={{
+        ...TransitionPresets.SlideFromRightIOS,
+        gestureEnabled: false,
+      }}>
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 
 export default App;
